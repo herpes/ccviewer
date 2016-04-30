a47faff7e25fc1804da65ed25be23478e6ffe91a
sohsuke nakamura <nakasonoaddress@gmail.com>
2016-04-30 17:43:37 +0900

    add parse_config.js

--- source/javascripts/parse_config.js
+++ source/javascripts/parse_config.js
@@ -48,7 +48,7 @@ interface FastEthernet1/0
  switchport 
  switchport mode trunk
  switchport trunk encapsulation dot1q
- switchport trunk allowed vlan 100
+ switchport trunk allowed vlan 100 ,110, 120 - 124
 !
 interface FastEthernet1/1
  switchport
@@ -64,6 +64,10 @@ interface FastEthernet2/0
  shutdown
  duplex auto
  speed auto
+!
+interface FastEthernet2/0.10
+ encapsulation dot1q 10
+ ip address 192.168.2.254 255.255.255.0
 !
 interface Vlan1
  no ip address
@@ -101,18 +105,115 @@ line vty 0 4
 end
 */}).toString().split('*')[1]
 
-//var RE_IF = /interface ()/;
 
-// console.log(CONFIG.split('\n')[0])
-mode = 0
-/*
-for(i in CONFIG.split('\n')){
-  if(/^!$/.exec(lines[i])){
-    // exit xx mode
-  }
-
-  if(/interface (.+)$/.exec(lines[i])){
-    // IF mode
-  }
+/* *********************************
+ *
+ * ********************************/
+function to_prefix(subnet){
+  prefix_list = subnet.split(".").map(function(elm){
+    return Number(elm).toString(2);
+  });
+  return prefix_list.join('').match(/^(1+)/)[1].length;
 }
-*/
+
+/* *********************************
+ *
+ * ********************************/
+function parse_if(c){
+  var interf = {};
+  var m = null;
+
+  // interface / port type(subinterface/svi/physical)
+  if(m = c.match(/^interface (.+)$/m)){
+    interf["interface"] = m[1];
+    if(/.+?\.\d+/.test(m[1])){
+      interf["type"] = "subinterface";
+    }else if(/vlan\s*\d+/i.test(m[1])){
+      interf["type"] = "svi";
+    }else{
+      interf["type"] = "physical";
+    }
+  }
+
+  // shutdown
+  interf["shutdown"] = /^\s+shutdown$/m.test(c);
+  
+  // description
+  if(m = c.match(/^\s+description (.+)$/m)){
+    interf["description"] = m[1];
+  }
+
+  // switchport
+  if(interf["type"] == "physical"){
+    if(m = c.match(/^\s+no switchport$/m)){
+      // do nothing
+    }else{
+      if(m = c.match(/^\s+switchport mode (.+)$/m)){
+        // access/trunk判定
+        interf["switchport"] = {"mode": m[1]};
+        if(m = c.match(/^\s+switchport trunk allowed vlan (.+)$/m)){
+          var vlans = m[1].replace(/\s/g, "").split(/,/);
+          interf["switchport"]["vlans"] = [];
+          for(i in vlans){
+            if(/-/.test(vlans[i])){
+              // 100-102の場合 100, 101, 102を追加する
+              var s = Number(vlans[i].split("-")[0]);
+              var e = Number(vlans[i].split("-")[1]);
+              for(s; s<e; s++){
+                interf["switchport"]["vlans"].push(s);
+              }
+            }else{
+              interf["switchport"]["vlans"].push(Number(vlans[i]));
+            }
+          }
+        }else if(m = c.match(/^\s+switchport access vlan (.+)$/m)){
+          interf["switchport"]["vlans"] = [Number(m[1])];
+        }
+      }
+    }
+  }
+
+  // ip address
+  if(m = c.match(/^\s+ip address (.+) (.+)$/m)){
+    interf["ip_address"] = {
+      "address": m[1],
+      "prefix": to_prefix(m[2])
+    }
+  }
+  
+  return interf;
+}
+
+// ---------------------------------
+// 返却値の作成
+// ---------------------------------
+var conf = {
+  "nodes": [],
+  "adjacencies": []
+}
+
+// ---------------------------------
+// node情報取得
+//   TODO: 本来は複数ファイルに対して実行する
+// ---------------------------------
+var node = {
+  "hostname": CONFIG.match(/^hostname (.+)$/m)[1],
+  "interfaces": []
+}
+
+// 
+var m = CONFIG.match(/interface [\s\S.]+?!/g);
+for(i in m){
+  node["interfaces"].push(parse_if(m[i]));
+}
+
+
+conf["nodes"].push(node);
+
+// ---------------------------------
+// 隣接判定
+//   TODO: どうするこれ
+// ---------------------------------
+
+
+console.log(JSON.stringify(conf));
